d83c12dbf42c94994ab6d6ba7b278b7c7b978d79
Shreya Gupta <creation9369778710@gmail.com>
2021-03-30 11:45:01 +0530

    Update and rename Chef and Division 3 to Chef-and-Division-3.cpp

--- Chef-and-Division-3.cpp
+++ Chef-and-Division-3.cpp
@@ -1,14 +1,3 @@
-Python
-
-for _ in range(int(input())):
-    N,K,D=  map(int,input().split())
-    A=map(int,input().split())
-    ans=sum(A)//K
-    print(ans if ans<=D else D)
-    
-    
-C++
-
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
