c469b6d141e10c9ed17d9ea7f3e90c770e35da0a
Shreya Gupta <creation9369778710@gmail.com>
2021-03-30 11:47:02 +0530

    Update and rename Encoded String to Encoded-String.cpp

--- Encoded-String.cpp
+++ Encoded-String.cpp
@@ -1,20 +1,3 @@
-https://www.codechef.com/JAN21C/problems/DECODEIT
-
-Python
-
-al=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p']
-for _ in range(int(input())):
-    w=""
-    p=0
-    n=int(input())
-    A=input()
-    for i in range(0,n//4):
-        c=A[((4*i)+0):((4*i)+4)]
-        p=int(c,2)
-        w+=al[p]
-    print(w)
-C++
-
 #include<iostream>
 #include<bits/stdc++.h>
 #include<algorithm>
